1a7ff6cdb2c9e0a0afc478d9cbd57ad44cddf638
anup-h <anup.h@berkeley.edu>
2020-02-15 10:49:14 -0800

    adding interactivity

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import flash, Flask, render_template, request
+from flask import flash, Flask, render_template, request, redirect
 from forms import QueryForm
 from flask_wtf.csrf import CSRFProtect
 import os
@@ -9,6 +9,8 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 csrf = CSRFProtect(app)
 
+search_string = ''
+
 SECRET_KEY = 'go bears'
 app.config['SECRET_KEY'] = SECRET_KEY
 
@@ -17,9 +19,28 @@ def home():
     search = QueryForm(request.form)
     if request.method == 'POST':
         search_string = request.form['query']
-        data = find_occurances(search_string, "andrew_ng.json")
-        return render_template('index.html', form=search, name=search_string, data=data)
+        return redirect('/'+search_string)
     return render_template('index.html', form=search)
+
+@app.route('/<search_term>', methods=('GET', 'POST'))
+def results(search_term):
+    data = find_occurances(search_term, "andrew_ng.json")
+    search = QueryForm(request.form)
+    if request.method == 'POST':
+        search_string = request.form['query']
+        return redirect('/'+search_string)
+    return render_template('index.html', form=search, name=search_term, data=data, showViewer=len(data)>0)
+
+# @app.route('/background_process_test')
+# def background_process_test():
+#     print ("Hello")
+#     return ("nothing")
+
+# @app.route('/background_process_test2')
+# def background_process_test2():
+#     print ("Goodbye")
+#     print(search_string)
+#     return ("nothing")
 
 if __name__ == "__main__":
     app.run(debug=True)
